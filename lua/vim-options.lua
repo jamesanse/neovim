180fa955edcb3c1f667f08b7e441058244fabd30
Jin An <jin.an@lookout.com>
2024-10-18 10:08:59 -0700

    adding quick pasting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- short code to copy text to system pasteboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+vim.keymap.set("n", "<leader>Y", [["+Y]])
